c460a896855cece8f7f39d0d64c5233ded785626
Cédric JUNG <cedced19@gmail.com>
2017-03-15 19:05:30 +0100

    Add middleware to compress and minify files

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var i18n = require('i18n-express');
+var compression = require('compression');
+var minifyTemplate = require('express-beautify').minify;
+var minifyCSS = require('express-minify');
 
 var index = require('./routes/index');
 var files = require('./routes/files-api');
@@ -15,12 +18,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+if (app.get('env') === 'development') {
+  app.use(logger('dev'));
+} else {
+  app.use(compression());
+  app.use(minifyTemplate());
+  app.use(minifyCSS());
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(i18n({
   translationsPath: path.join(__dirname, 'i18n'),
